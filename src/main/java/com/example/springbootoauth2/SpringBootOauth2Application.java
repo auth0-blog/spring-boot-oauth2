fe6b65fcebb8c72fddae64925953348d9d59905f
Bruno Krebs <bruno.krebs@auth0.com>
2018-10-25 21:29:36 -0300

    fixing indentation;

--- src/main/java/com/example/springbootoauth2/SpringBootOauth2Application.java
+++ src/main/java/com/example/springbootoauth2/SpringBootOauth2Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootOauth2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootOauth2Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootOauth2Application.class, args);
+  }
 }
